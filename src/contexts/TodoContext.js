df3ea946c774426f6ac34e562f8d296f5c984b19
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-03-24 21:33:44 +0500

    Fix Small bugs

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -8,14 +8,14 @@ export const TodoContext = createContext({
             completed: false,
         }
     ],
-    addTodo: (todo) => {},
-    updateTodo: (id, todo) => {},
-    deleteTodo: (id) => {},
-    toggleComplete: (id) => {},
+    addTodo: (todo) => { },
+    updateTodo: (id, todo) => { },
+    deleteTodo: (id) => { },
+    toggleComplete: (id) => { },
 })
 
 export const useTodo = () => {
-  return useContext(TodoContext)
+    return useContext(TodoContext)
 }
 
 export const TodoProvider = TodoContext.Provider
